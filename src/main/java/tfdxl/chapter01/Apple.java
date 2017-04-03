a0e027b7ad8cb658f6a3a5a7abb2d782f9859acf
tianfeng <tianfeng@souche.com>
2017-04-03 21:51:12 +0800

    add

---
+++ src/main/java/tfdxl/chapter01/Apple.java
@@ -0,0 +1,19 @@
+package tfdxl.chapter01;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by tianfeng on 2017/4/3.
+ */
+@Data
+@AllArgsConstructor
+public class Apple {
+
+    //name
+    private String name;
+
+    //unit:g
+    private int weight;
+
+}
---
+++ src/main/java/tfdxl/chapter01/StreamProcessing01.java
@@ -0,0 +1,27 @@
+package tfdxl.chapter01;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by tianfeng on 2017/4/3.
+ */
+public class StreamProcessing01 {
+
+    public static void main(String[] args) {
+
+        List<Apple> inventory = new ArrayList();
+
+        for(int i=0;i<10;i++){
+            inventory.add(new Apple("apple"+i,i));
+        }
+
+        //顺序处理
+        List heavyApples1 = inventory.stream().filter((Apple apple) ->apple.getWeight()>4).collect(Collectors.toList());
+
+        //并行处理
+        List<Apple> heavyApple2 = inventory.parallelStream().filter((Apple apple) ->apple.getWeight()>4).collect(Collectors.toList());
+    }
+}
